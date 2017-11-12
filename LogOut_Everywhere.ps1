eb8b6bd15af28bfe2423b30be78eaec98f52bb9b
Pratik Thakkar <thakkarpratik277@gmail.com>
2017-11-13 02:01:27 +0530

    Update LogOut_Everywhere.ps1

--- LogOut_Everywhere.ps1
+++ LogOut_Everywhere.ps1
@@ -2,7 +2,7 @@ $allmachines=Get-ADComputer -Filter * | Select -Expand Name
 
 foreach ($server In $allmachines) {
 
-#$server   = 'a31b3ch3dms01'
+#$server   = ''
 $username = 'MyUserName'
 
 $session = ((quser MyUserName /server:$server | ? { $_ -match $username }) -split ' +')[2]
