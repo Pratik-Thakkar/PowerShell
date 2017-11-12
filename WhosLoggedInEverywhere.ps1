a551408c6a66f052e43282c6ede672c31e65c66e
Pratik Thakkar <thakkarpratik277@gmail.com>
2017-11-13 01:59:49 +0530

    Update WhosLoggedInEverywhere.ps1

--- WhosLoggedInEverywhere.ps1
+++ WhosLoggedInEverywhere.ps1
@@ -2,8 +2,8 @@ $allmachines=Get-ADComputer -Filter * | Select -Expand Name
 
 foreach ($server In $allmachines) {
 
-#$server   = 'a31b3ch3dms01'
-#$username = 'chris.bier'
+#$server   = ''
+#$username = ''
 
 $session = (quser /server:$server | findstr /V "USERNAME")
 echo $server
